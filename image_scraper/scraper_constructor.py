489201af51f8d9249692564c583f4ee79c44e245
WebLudus <webluduspl+github@gmail.com>
2022-10-13 19:48:46 +0200

    Added docstring into create_image_scraper

--- image_scraper/scraper_constructor.py
+++ image_scraper/scraper_constructor.py
@@ -12,8 +12,20 @@ def create_image_scraper(
     container_class: str,
     pagination_class: str,
     pages_to_scan: int,
-    scraper: str,
+    scraper: str = "bs4"
 ) -> ImageScraper:
+    """Constructor for the ImageScraper object. Imports scrapers and delivers them to
+    ImageScraper based on the information in the scraper variable.
+
+    Args:
+        website_url: the URL address of website to scan.
+        container_class: a class of div or section element containing image
+        pagination_class: a class of div or section element containing pagination
+            URLs.
+        pages_to_scan: how many pages should be scraped.
+        scraper: tool to be used.
+
+    Returns: the ImageScraper object."""
     if scraper in SCRAPERS:
         return ImageScraper(
             website_url=website_url,
