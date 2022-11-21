b56a0ccb1f82451237daf86f60877a98926c2b78
WebLudus <webluduspl+github@gmail.com>
2022-11-21 14:43:59 +0100

    Added missing information in docstring.

--- image_scraper/src/scrapers/scraper.py
+++ image_scraper/src/scrapers/scraper.py
@@ -11,12 +11,15 @@ class Scraper(ABC):
         self, image_source: ImagesSource, last_sync_data: tuple[str] | tuple[()] = ()
     ) -> tuple[set[Image], bool]:
         """The method that starts the synchronization process.
+        If, during synchronization, encounters an image located in last_sync_data,
+        it stops synchronization and returns True as the second argument.
+        If the synchronization is complete, the second argument will be False.
 
         Args:
             image_source: the ImagesSource object. Contains website data.
             last_sync_data: URLs of recently downloaded images (img_src).
 
-        Returns: set containing the Image objects."""
+        Returns: a tuple in which there is a set with Image objects and bool."""
         pass
 
     @abstractmethod
