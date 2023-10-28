ad299859bd79ddd2479ee5c68465efbc81c6a58e
Dawid Szaniawski <98256512+dawid-szaniawski@users.noreply.github.com>
2023-10-28 21:55:40 +0200

    chore: release 0.2.0
    * feat: add Image to __all__

--- imgscraper/__init__.py
+++ imgscraper/__init__.py
@@ -2,10 +2,12 @@
 from logging import NullHandler, getLogger
 
 from .scraper_constructor import create_scraper
+from .src.models import Image
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "create_scraper",
+    "Image",
 ]
 
 getLogger(__name__).addHandler(NullHandler())
