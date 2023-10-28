12c3de35dc53e2048f905d51d001626bfb6cb996
Dawid Szaniawski <www.dszaniawski@gmail.com>
2023-10-28 21:37:15 +0200

    fix: depedency fix

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ REQUIRED_PYTHON = (3, 10)
 
 if sys.version_info[:2] < REQUIRED_PYTHON:
     sys.stderr.write(
-        """\nUnsupported Python version\n.
-        This version of ImgScraper requires at least Python {}.{}""".format(
-            *REQUIRED_PYTHON
-        )
+        f"""\nUnsupported Python version\n.
+        This version of ImgScraper requires at least
+         Python {REQUIRED_PYTHON[0]}.{REQUIRED_PYTHON[1]}"""
     )
     sys.exit(1)
 
